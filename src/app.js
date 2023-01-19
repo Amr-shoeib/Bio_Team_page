29862df68f877cd48c5a758bc30a0f1678bfe505
Amr-shoeib <amr79.a@gmail.com>
2023-01-18 22:36:54 -0500

    link index file to the css
    link public directory to CSS and js

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const publicDirectoryPath = path.join(__dirname, '../Public');
+app.use(express.static(publicDirectoryPath));
 
 app.set('view engine', 'hbs');
 
