5a5964c98f933d6bddd149e61a87394bdf8fd563
Amr-shoeib <amr79.a@gmail.com>
2023-01-18 21:34:53 -0500

    install nodemon

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express()
 
 app.get('/', function (req, res) {
-  res.send('<h1>Hello World</h1>')
+  res.send('<h1>Hello World from home page</h1>')
 })
 
 app.get('/about', function (req, res) {
